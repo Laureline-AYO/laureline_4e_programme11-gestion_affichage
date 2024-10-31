b17000c24ecd5e595aa8edc24ad2b80a985852c5
Laureline-AYO <layla.cappy@outlook.com>
2024-10-31 15:56:15 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
-input.onGesture(Gesture.LogoUp, function () {
-    mode += 1
-    if (mode == 1) {
-        basic.showString("Heure")
-    }
-    if (mode == 2) {
-        basic.showString("Pas")
-    }
-    if (mode == 3) {
-        basic.showString("Distance")
-    }
-    if (mode == 4) {
-        basic.showString("Temperature")
-    }
-    if (mode == 5) {
-        basic.showString("Boussole")
+let mode = 1
+basic.forever(function () {
+    if (input.isGesture(Gesture.LogoUp)) {
+        mode += 1
+        if (mode == 1) {
+            basic.showString("Heure")
+        }
+        if (mode == 2) {
+            basic.showString("Pas")
+        }
+        if (mode == 3) {
+            basic.showString("Distance")
+        }
+        if (mode == 4) {
+            basic.showString("Temperature")
+        }
+        if (mode == 5) {
+            basic.showString("Boussole")
+        }
     }
 })
-let mode = 0
-mode = 0
